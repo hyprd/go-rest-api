5daeee4728cb4318ece89e67caa161d012667857
hyprd <yumether@gmail.com>
2020-07-31 14:22:42 +1200

    Route handlers and Book struct created

--- main.go
+++ main.go
@@ -8,4 +8,10 @@ func main() {
 	// Initialize router
 	r := mux.NewRouter()
 
+	// Route handlers
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 }
